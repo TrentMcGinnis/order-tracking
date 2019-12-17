6fee6a5d300903cf3632826376aee03111c4615e
Trent McGinnis <mcginntm@mail.uc.edu>
2019-12-17 10:57:36 -0800

    token header requires 'Bearer' now

--- backend/Middleware/Auth.js
+++ backend/Middleware/Auth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (req,res,next) => {
   try {
-    const token = req.headers.authorization;
+    const token = req.headers.authorization.split(" ")[1];
     const decodedToken = jwt.verify(token, process.env.TOKEN_SIGN);
     req.userData = {email: decodedToken.email, id: decodedToken.userid};
     next();
